0c8eee2325ed117094e74726d5e88020cabf4a9a
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-06 21:51:30 +0900

    Add requirements.txt, setup-env.sh

---
+++ setup-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# basic
+mamba create -n GoLLIE python=3.10 -y; mamba activate GoLLIE
+mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y;
+
+pip install -U -r requirements.txt
+pip install flash-attn==2.0.2 --no-build-isolation
