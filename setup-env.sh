0389394a227a205d6099f1d86e3ca34738fa5e6f
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-07 15:44:51 +0900

    testing MAX_JOBS

--- setup-env.sh
+++ setup-env.sh
@@ -4,4 +4,4 @@ mamba create -n GoLLIE python=3.11 -y; mamba activate GoLLIE;
 mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y;
 
 pip install -U -r requirements.txt
-pip install flash-attn==2.0.4
+MAX_JOBS=12 pip install flash-attn==2.0.4 --no-build-isolation
