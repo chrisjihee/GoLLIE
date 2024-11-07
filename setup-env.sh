98a7180547fb96e111b440234df78f2ae7709fb9
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-07 19:07:48 +0900

    Update minor

--- setup-env.sh
+++ setup-env.sh
@@ -2,6 +2,5 @@
 # basic
 mamba create -n GoLLIE python=3.11 -y; mamba activate GoLLIE;
 mamba install cuda-nvcc=11.8 cudatoolkit=11.8 -c nvidia -y;
-
-pip install -U -r requirements.txt
-MAX_JOBS=12 pip install flash-attn==2.0.4 --no-build-isolation
+pip install -U -r requirements.txt;
+MAX_JOBS=12 pip install flash-attn==2.0.4 --no-build-isolation;
